cd8ff28eba7e18eb983d8deea03e406b27297157
Utkarsh <tu858371@gmail.com>
2022-07-16 05:18:37 +0530

    Port rectified

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.all('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Server running at port ${port}`);
 })
 
